d2767b7f77743a4e95a3a3b026421d5bc9ac43d2
cmarvann <covingtonmarvann@gmail.com>
2022-08-22 02:26:33 -0400

    finanlize project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
+// const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+// const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -127,15 +129,113 @@ Add a New Project
 };
 
 
-// Function Call
+// // Function Call
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     console.log(portfolioData);
+//     const pageHTML = generatePage(portfolioData);
+
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//       if (err) {
+//       console.log(err);
+//       return;
+//     }
+//       console.log('Page created! Check out index.html in this directory to see it!');
+
+      
+
+//     });
+//   });
+
+
+// Promises
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+
+
+
+
+// const mockData = {
+//     name: 'MarvanCovington',
+//     github: 'Marvannhub',
+//         confirmAbout: true,
+//         about: 'I love coding ',
+//         projects: [
+//           {
+//             name: 'Professional -Portfolio-Generator',
+//             description: 'Portfolio-Generator',
+//             languages: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'],
+//             link: 'https://github.com/cmarvann/Marvann.git',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'BootCamp-Code-Quizer',
+//             description: 'Test-Knowledge',
+//             languages: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap'],
+//             link: 'https://github.com/cmarvann/BootCamp-Code-Quizer.git',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Run-Buddy',
+//             description: 'Training-Site',
+//             languages: ['HTML', 'CSS'],
+//             link: 'https://github.com/cmarvann/Runn-Buddy.git',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Robot-Gladiators',
+//             description: 'Game',
+//             languages: ['Javascript'],
+//             link: 'https://github.com/cmarvann/Robot-',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Taskmaster-Pro',
+//             description: 'create-task',
+//             languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+//             link: 'https://github.com/cmarvann/taskmaster-pro.git',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Weather-App',
+//             description: 'Five-Day-Weather-Forecast',
+//             languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+//             link: 'https://github.com/cmarvann/weather-dashboard.git',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Just-Tech-News',
+//             description: 'Articles',
+//             languages: ['Node'],
+//             link: 'https://github.com/cmarvann/just-tech-news.git',
+//             feature: false,
+//             confirmAddProject: false
+//           }
+//         ]
+       
+//   }
+//   const pageHTML = generatePage(mockData);
---
+++ test/sample-answers.js
@@ -0,0 +1,67 @@
+module.exports = {
+    
+        name: 'Marvann',
+        github: 'cmarvann',
+        confirmAbout: true,
+        about: 'I lov ecoding ',
+        projects: [
+          {
+            name: 'Professional -Portfolio-Generator',
+            description: 'Portfolio-Generator',
+            languages: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'],
+            link: 'https://github.com/cmarvann/Marvann.git',
+            feature: true,
+            confirmAddProject: true
+          },
+          {
+            name: 'BootCamp-Code-Quizer',
+            description: 'Test-Knowledge',
+            languages: [Array],
+            link: 'https://github.com/cmarvann/BootCamp-Code-Quizer.git',
+            feature: true,
+            confirmAddProject: true
+          },
+          {
+            name: 'Run-Buddy',
+            description: 'Training-Site',
+            languages: ['HTML', 'CSS'],
+            link: 'https://github.com/cmarvann/Runn-Buddy.git',
+            feature: true,
+            confirmAddProject: true
+          },
+          {
+            name: 'Robot-Gladiators',
+            description: 'Game',
+            languages: ['Jvascript','HTML', 'CSS'],
+            link: 'https://github.com/cmarvann/Robot-',
+            feature: true,
+            confirmAddProject: true
+          },
+          {
+            name: 'Taskmaster-Pro',
+            description: 'create-task',
+            languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+            link: 'https://github.com/cmarvann/taskmaster-pro.git',
+            feature: true,
+            confirmAddProject: true
+          },
+          {
+            name: 'Weather-App',
+            description: 'Five-Day-Weather-Forecast',
+            languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+            link: 'https://github.com/cmarvann/weather-dashboard.git',
+            feature: true,
+            confirmAddProject: true
+          },
+          {
+            name: 'Just-Tech-News',
+            description: 'Articles',
+            languages: ['Node'],
+            link: 'https://github.com/cmarvann/just-tech-news.git',
+            feature: false,
+            confirmAddProject: false
+          }
+        ]
+      
+      
+};
